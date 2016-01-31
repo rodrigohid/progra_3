002b7cc55cb86d20d0f6b7ee4c03c282d9ab7b4d
rodrigohid <rodrigohid@gmail.com>
2016-01-30 18:47:27 -0600

    clase3
    clase3

---
+++ Clase_3/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Clase_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtdato = new System.Windows.Forms.TextBox();
+            this.txtBool = new System.Windows.Forms.TextBox();
+            this.txtnum = new System.Windows.Forms.TextBox();
+            this.txtPalabra = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Silver;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(115, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Datos";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(55, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Llenar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(173, 216);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Salir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtdato
+            // 
+            this.txtdato.Location = new System.Drawing.Point(32, 84);
+            this.txtdato.Name = "txtdato";
+            this.txtdato.Size = new System.Drawing.Size(100, 20);
+            this.txtdato.TabIndex = 3;
+            // 
+            // txtBool
+            // 
+            this.txtBool.Location = new System.Drawing.Point(148, 84);
+            this.txtBool.Name = "txtBool";
+            this.txtBool.Size = new System.Drawing.Size(100, 20);
+            this.txtBool.TabIndex = 4;
+            // 
+            // txtnum
+            // 
+            this.txtnum.Location = new System.Drawing.Point(32, 127);
+            this.txtnum.Name = "txtnum";
+            this.txtnum.Size = new System.Drawing.Size(100, 20);
+            this.txtnum.TabIndex = 5;
+            // 
+            // txtPalabra
+            // 
+            this.txtPalabra.Location = new System.Drawing.Point(148, 127);
+            this.txtPalabra.Name = "txtPalabra";
+            this.txtPalabra.Size = new System.Drawing.Size(100, 20);
+            this.txtPalabra.TabIndex = 6;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(90, 174);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(100, 20);
+            this.txt3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
+            this.ClientSize = new System.Drawing.Size(291, 298);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txtPalabra);
+            this.Controls.Add(this.txtnum);
+            this.Controls.Add(this.txtBool);
+            this.Controls.Add(this.txtdato);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "* *  Menu  * *";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtdato;
+        private System.Windows.Forms.TextBox txtBool;
+        private System.Windows.Forms.TextBox txtnum;
+        private System.Windows.Forms.TextBox txtPalabra;
+        private System.Windows.Forms.TextBox txt3;
+    }
+}
+
---
+++ Clase_3/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clase_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtdato.Text = "Hola Mundo";
+            bool content = true;
+            txtBool.Text = content.ToString();
+            int n = 88;
+            txtnum.Text = n.ToString();
+            string palabra = "palabra/string";
+            txtPalabra.Text = palabra;
+            int[] arreglo = { 5, 2, 3 };
+
+
+            foreach (int gg in arreglo)
+            {
+              txt3.Text = txt3.Text + gg.ToString() + "  ";
+
+
+            }
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+    }
+}
